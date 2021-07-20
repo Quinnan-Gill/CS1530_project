04088aa4660972a9100ebfcfe1ae7dcf5e9376c2
Quinnan Gill <quinnan.gill@gmail.com>
2021-07-20 17:15:35 -0600

    Added player and tiles

--- Assets/Scripts/PlatformerCharacter2D.cs
+++ Assets/Scripts/PlatformerCharacter2D.cs
@@ -21,8 +21,6 @@ namespace UnityStandardAssets._2D
         private Rigidbody2D m_Rigidbody2D;
         private bool m_FacingRight = true;  // For determining which way the player is currently facing.
 
-        Transform playerGraphics;           // Reference to the graphics so we can change direction
-
         private void Awake()
         {
             // Setting up references.
@@ -30,12 +28,6 @@ namespace UnityStandardAssets._2D
             m_CeilingCheck = transform.Find("CeilingCheck");
             m_Anim = GetComponent<Animator>();
             m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
-            playerGraphics = transform.Find("Graphics");
-            if (playerGraphics == null)
-            {
-                Debug.LogError("Let's freak out! There is no 'Graphics' object as a child of the player");
-            }
         }
 
 
@@ -115,9 +107,9 @@ namespace UnityStandardAssets._2D
             m_FacingRight = !m_FacingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = playerGraphics.localScale;
+            Vector3 theScale = transform.localScale;
             theScale.x *= -1;
-            playerGraphics.localScale = theScale;
+            transform.localScale = theScale;
         }
     }
 }
