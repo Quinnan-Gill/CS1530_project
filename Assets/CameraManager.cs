0aea0273e04d90bd55d07cf411fa6e1d386d5318
Quinnan Gill <quinnan.gill@gmail.com>
2021-07-26 20:13:30 -0600

    Added camera tracking

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -7,27 +7,51 @@ public class CameraManager : MonoBehaviour
     public GameObject[] cam_transforms;
     public Transform player;
 
-    private Canvas[] canvas_list;
+    private Transform[] canvas_list;
 
     // Start is called before the first frame update
     void Awake()
     {
-        canvas_list = new Canvas[cam_transforms.Length];
+        canvas_list = new Transform[cam_transforms.Length];
+
         // For loop that gets all the cameras canvases
-        // for(int i=0; i < canvas_list.Length; i++)
-        // {
-        //     canvas_list[i] = cam_transforms[i].GetComponent<NewScript>().cameraCanvas;
-        // }
+        for(int i=0; i < canvas_list.Length; i++)
+        {
+            canvas_list[i] = cam_transforms[i].GetComponent<Camera2DFollow>().cameraCanvas;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for(int i=0; i < canvas_list.Length; i++)
+        {
+            if (WithinCanvas(canvas_list[i]))
+            {
+                cam_transforms[i].GetComponent<Camera>().enabled = true;
+            }
+            else
+            {
+                cam_transforms[i].GetComponent<Camera>().enabled = false;
+            }
+        }
     }
 
-    private bool WithinCanvas()
+    private bool WithinCanvas(Transform canvas)
     {
+        float canvas_height = canvas.GetComponent<RectTransform>().rect.height;
+        float canvas_width  = canvas.GetComponent<RectTransform>().rect.width;
+
+        if (
+            (player.position.x >= canvas.position.x - canvas_width/2)  &&
+            (player.position.x <  canvas.position.x + canvas_width/2)  &&
+            (player.position.y >= canvas.position.y - canvas_height/2) &&
+            (player.position.y <  canvas.position.y + canvas_height/2)
+        )
+        {
+            return true;
+        }
+        
         return false;
     }
 }
--- Assets/Scripts/Camera2DFollow.cs
+++ Assets/Scripts/Camera2DFollow.cs
@@ -1,86 +1,83 @@
 using System;
 using UnityEngine;
 
-namespace UnityStandardAssets._2D
+public class Camera2DFollow : MonoBehaviour
 {
-    public class Camera2DFollow : MonoBehaviour
+    public Transform target;
+    public float damping = 1;
+    public float lookAheadFactor = 3;
+    public float lookAheadReturnSpeed = 0.5f;
+    public float lookAheadMoveThreshold = 0.1f;
+
+    private float m_OffsetZ;
+    private Vector3 m_LastTargetPosition;
+    private Vector3 m_CurrentVelocity;
+    private Vector3 m_LookAheadPos;
+
+    public Transform cameraCanvas;
+    private float cam_height;
+    private float cam_width;
+    private float canvas_height;
+    private float canvas_width;
+
+    // Use this for initialization
+    private void Start()
     {
-        public Transform target;
-        public float damping = 1;
-        public float lookAheadFactor = 3;
-        public float lookAheadReturnSpeed = 0.5f;
-        public float lookAheadMoveThreshold = 0.1f;
+        m_LastTargetPosition = target.position;
+        m_OffsetZ = (transform.position - target.position).z;
+        transform.parent = null;
 
-        private float m_OffsetZ;
-        private Vector3 m_LastTargetPosition;
-        private Vector3 m_CurrentVelocity;
-        private Vector3 m_LookAheadPos;
+        Camera cam = gameObject.GetComponent<Camera>();
+        cam_height = 2f * cam.orthographicSize;
+        cam_width = cam_height * cam.aspect;
 
-        public Transform cameraCanvas;
-        private float cam_height;
-        private float cam_width;
-        private float canvas_height;
-        private float canvas_width;
+        canvas_height = cameraCanvas.GetComponent<RectTransform>().rect.height;
+        canvas_width  = cameraCanvas.GetComponent<RectTransform>().rect.width;
+    }
 
-        // Use this for initialization
-        private void Start()
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // only update lookahead pos if accelerating or changed direction
+        float xMoveDelta = (target.position - m_LastTargetPosition).x;
+
+        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+        if (updateLookAheadTarget)
         {
-            m_LastTargetPosition = target.position;
-            m_OffsetZ = (transform.position - target.position).z;
-            transform.parent = null;
-
-            Camera cam = gameObject.GetComponent<Camera>();
-            cam_height = 2f * cam.orthographicSize;
-            cam_width = cam_height * cam.aspect;
-
-            canvas_height = cameraCanvas.GetComponent<RectTransform>().rect.height;
-            canvas_width  = cameraCanvas.GetComponent<RectTransform>().rect.width;
+            m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
+        }
+        else
+        {
+            m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
         }
 
+        Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
+        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
-        // Update is called once per frame
-        private void Update()
+        if (cameraCanvas != null)
         {
-            // only update lookahead pos if accelerating or changed direction
-            float xMoveDelta = (target.position - m_LastTargetPosition).x;
-
-            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
-
-            if (updateLookAheadTarget)
-            {
-                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
-            }
-            else
-            {
-                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
-            }
-
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
-            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
-
-            if (cameraCanvas != null)
-            {
-                transform.position = new Vector3(
-                    Mathf.Clamp(
-                        newPos.x,
-                        cameraCanvas.position.x - canvas_width/2 + cam_width/2,
-                        cameraCanvas.position.x + canvas_width/2 - cam_width/2
-                    ),
-                    // newPos.x,
-                    Mathf.Clamp(
-                        newPos.y,
-                        cameraCanvas.position.y - canvas_height/2 + cam_height/2,
-                        cameraCanvas.position.y + canvas_height/2 - cam_height/2
-                    ),
-                    newPos.z
-                );
-            }
-            else
-            {
-                transform.position = newPos;
-            }
-
-            m_LastTargetPosition = target.position;
+            transform.position = new Vector3(
+                Mathf.Clamp(
+                    newPos.x,
+                    cameraCanvas.position.x - canvas_width/2 + cam_width/2,
+                    cameraCanvas.position.x + canvas_width/2 - cam_width/2
+                ),
+                // newPos.x,
+                Mathf.Clamp(
+                    newPos.y,
+                    cameraCanvas.position.y - canvas_height/2 + cam_height/2,
+                    cameraCanvas.position.y + canvas_height/2 - cam_height/2
+                ),
+                newPos.z
+            );
         }
+        else
+        {
+            transform.position = newPos;
+        }
+
+        m_LastTargetPosition = target.position;
     }
 }
