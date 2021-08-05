ea0a502fe46bf21df8f9328b582ddb3fed7fee32
Pittstudent <rtg18@pitt.edu>
2021-08-05 02:08:18 -0400

    added hearts

---
+++ Assets/HealthHeartSystem/Scripts/Health.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public int health;
+   public int numofHearts;
+
+   public Image [] hearts;
+   public Sprite fullHeart;
+   public Sprite emptyHeart;
+
+   void Update(){
+   	if(health>numofHearts)
+   	health = numofHearts;
+   	for(int i = 0; i<hearts.Length;i++) {
+   		if(i<health){
+   			hearts[i].sprite=fullHeart;
+   		} else{
+   			hearts[i].sprite=emptyHeart;
+   				}
+
+   		if(i<numofHearts){
+   			hearts[i].enabled=true;
+   		} else{
+   			hearts[i].enabled=true;
+   				}
+   		}
+
+  	}
+}
